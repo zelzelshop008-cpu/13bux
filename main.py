f3f11d43edaaaca87d08375e877cf2566f774bba
bambooggxd-del <bambooggxd@gmail.com>
2026-07-29 21:13:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3649,7 +3649,7 @@ async def ae(ctx):
 
     @bot.command()
 async def mine(ctx):
-    await ctx.send("Expedition เข้าเซิฟนี้มานะคะ ถ้าเข้าไม่ได้บอกนะ https://www.roblox.com/share?code=7d70653c7eb7724283cba0fcea6d6701&type=Server")
+    await ctx.send("Mine เข้าเซิฟนี้มานะคะ ถ้าเข้าไม่ได้บอกนะ https://www.roblox.com/share?code=7d70653c7eb7724283cba0fcea6d6701&type=Server")
     
 # ============ BACKGROUND TASKS ============
 @tasks.loop(minutes=1)
